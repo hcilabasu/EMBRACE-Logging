bd2c8dfa809fa74eb36e46f5bc5941ba8c07c06b
Audrey Wong <aewong@asu.edu>
2016-11-08 13:32:32 -0700

    Fixed path issues with log_preprocessor

--- log_preprocessor.py
+++ log_preprocessor.py
@@ -1,34 +1,43 @@
 import os
 import re
 
-for root, dirs, files in os.walk("."):
+# Check that path is correct before running this script!
+PATH_TO_LOG_FILES = "."
+
+for root, dirs, files in os.walk(PATH_TO_LOG_FILES):
     for dir in dirs:
-        for file in os.listdir(dir):
-            newFile = file.replace(".txt", ".xml")
+        dirPath = os.path.join(root, dir)
+        
+        for file in os.listdir(dirPath):
+            if file != ".DS_Store" and ".txt" in file:
+                newFile = file.replace(".txt", ".xml")
 
-            iFile = open(os.path.join(dir, file), "r")
-            oFile = open(os.path.join(dir, newFile), "w")
-
-            lineNumber = 0
-
-            for line in iFile:
-                if lineNumber != 0:
-                    # Find index of first > symbol
-                    openR = line.find(">")
-                    
-                    # Replace spaces inside opening XML tag with underscores
-                    line = re.sub(r'([^\s])\s([^\s])', r'\1_\2', line[:openR]) + line[openR:]
-                    
-                    # Find index of second < symbol
-                    closeL = line.rfind("<")
-                    
-                    # Replace sapces inside closing XML tag with underscores
-                    line = line[:closeL] + line[closeL:].replace(" ", "_")
+                filePath = os.path.join(dirPath, file)
+                newFilePath = os.path.join(dirPath, newFile)
                 
-                # Write modified line to file
-                oFile.write(line)
+                iFile = open(filePath, "r")
+                oFile = open(newFilePath, "w")
 
-                lineNumber += 1
+                lineNumber = 0
 
-            iFile.close()
-            oFile.close()
+                for line in iFile:
+                    if lineNumber != 0:
+                        # Find index of first > symbol
+                        openR = line.find(">")
+                        
+                        # Replace spaces inside opening XML tag with underscores
+                        line = re.sub(r'([^\s])\s([^\s])', r'\1_\2', line[:openR]) + line[openR:]
+                        
+                        # Find index of second < symbol
+                        closeL = line.rfind("<")
+                        
+                        # Replace sapces inside closing XML tag with underscores
+                        line = line[:closeL] + line[closeL:].replace(" ", "_")
+                    
+                    # Write modified line to file
+                    oFile.write(line)
+
+                    lineNumber += 1
+
+                iFile.close()
+                oFile.close()
